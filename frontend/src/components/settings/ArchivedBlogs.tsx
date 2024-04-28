db1eeac67b89f2619d74ade4fed63693bff5e7cc
Hairum Qureshi <hqureshi@udel.edu>
2024-04-28 19:43:20 -0400

    began working on ArchivedBlogs component by displaying the archived blogs that user has

--- frontend/src/components/settings/ArchivedBlogs.tsx
+++ frontend/src/components/settings/ArchivedBlogs.tsx
@@ -1,9 +1,19 @@
 import settings_css from "./../../css/settings.module.css";
 import useAuthContext from "../../contexts/authContext";
 import NotFound from "../NotFound";
+import useProfileData from "../../hooks/useProfileData";
+import { useEffect } from "react";
+import { Blog } from "../../interfaces";
 
 export default function ArchivedBlogs() {
 	const { userData } = useAuthContext()!;
+	const { getProfileData, blogs } = useProfileData();
+
+	useEffect(() => {
+		if (userData) {
+			getProfileData(userData.user_id);
+		}
+	}, []);
 
 	return userData && userData.message !== "user does not exist" ? (
 		<>
@@ -11,7 +21,20 @@ export default function ArchivedBlogs() {
 				<div className={settings_css.header}>
 					<h3>Your Archived Blogs</h3>
 				</div>
-				{userData.archived_blogs.length === 0 ? (
+				{blogs && blogs.length > 0
+					? blogs.map((blog: Blog) => {
+							return blog.archived ? (
+								<div className={settings_css.section}>
+									<div>
+										<h3>{blog.blog_title}</h3>
+										<p>{blog.blog_summary}</p>
+										<button>Unarchive</button>
+									</div>
+								</div>
+							) : null;
+					  })
+					: null}
+				{/* {userData.archived_blogs.length === 0 ? (
 					<>
 						<div className={settings_css.section}>
 							You currently don't have any blogs archived.
@@ -21,7 +44,7 @@ export default function ArchivedBlogs() {
 					userData.archived_blogs.map(blog => {
 						return <div className={settings_css.section}>{blog}</div>;
 					})
-				)}
+				)} */}
 			</div>
 		</>
 	) : (
