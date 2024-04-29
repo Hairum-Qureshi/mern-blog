f19d77462acc704050aea16b6511c70611dbae25
Hairum Qureshi <hqureshi@udel.edu>
2024-04-29 19:05:38 -0400

    added logic to filter blogs that have not been archived and improved logic to display the message if all the blogs are really archived

--- frontend/src/components/profile/Blogs.tsx
+++ frontend/src/components/profile/Blogs.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useAuthContext from "../../contexts/authContext";
 import useProfileData from "../../hooks/useProfileData";
 import { useSettings } from "../../hooks/useSettings";
@@ -19,6 +19,7 @@ export default function Blogs() {
 	const { user_id } = useParams();
 	const { getProfileData, userProfileData, blogs, archiveBlog } =
 		useProfileData();
+	const [nonArchivedBlogs, setNonArchivedBlogs] = useState<Blog[]>([]);
 
 	useEffect(() => {
 		if (user_id) {
@@ -26,12 +27,19 @@ export default function Blogs() {
 		}
 	}, [user_id]);
 
+	useEffect(() => {
+		if (blogs) {
+			const nonArchived_blogs: Blog[] = blogs.filter((blog: Blog) => {
+				return !blog.archived;
+			});
+			setNonArchivedBlogs(nonArchived_blogs);
+		}
+	}, []);
+
 	const navigate = useNavigate();
 
 	return (
 		// TODO - need to add logic to hide the buttons if the user visits another user's profile page's blog tab
-		// TODO - fix the logic where it only displays the message "all of your blogs are archived" if all the blogs are archived
-		//		-> may need to use filter
 		// TODO - need to change the 'active' styling when you click the 'here' link text
 		<>
 			<div className={profile_css.blogsContainer}>
@@ -39,74 +47,80 @@ export default function Blogs() {
 					<input type="search" placeholder="Search by title" />
 				</div>
 				<div className={profile_css.blogs}>
-					{(blogs &&
-						blogs.length > 0 &&
-						blogs.map((blog: Blog) =>
-							!blog.archived ? (
-								<>
-									<div
-										className={profile_css.container}
-										onClick={() =>
-											navigate(
-												`/blogs/${blog.route_id}/${blog.sanitized_title}`
-											)
-										}
-										key={Math.floor(Math.random() * Date.now())}
-									>
-										<h2>{blog.blog_title.toUpperCase()}</h2>
-										{blog.published ? (
-											<p className={profile_css.statusFlair_published}>
-												PUBLISHED
-											</p>
-										) : (
-											<p className={profile_css.statusFlair_unpublished}>
-												UNPUBLISHED
-											</p>
-										)}
-										<div className={profile_css.summaryContainer}>
-											<p>{blog.blog_summary}</p>
+					{
+						nonArchivedBlogs.length > 0 ? (
+							nonArchivedBlogs.map((blog: Blog) => {
+								return (
+									<>
+										<div
+											className={profile_css.container}
+											onClick={() =>
+												navigate(
+													`/blogs/${blog.route_id}/${blog.sanitized_title}`
+												)
+											}
+											key={Math.floor(Math.random() * Date.now())}
+										>
+											<h2>{blog.blog_title.toUpperCase()}</h2>
+											{blog.published ? (
+												<p className={profile_css.statusFlair_published}>
+													PUBLISHED
+												</p>
+											) : (
+												<p className={profile_css.statusFlair_unpublished}>
+													UNPUBLISHED
+												</p>
+											)}
+											<div className={profile_css.summaryContainer}>
+												<p>{blog.blog_summary}</p>
+											</div>
+											<div className={profile_css.buttonGroup}>
+												<button
+													title="Archive"
+													onClick={e => {
+														e.stopPropagation();
+														archiveBlog(userData?.user_id, blog.route_id);
+													}}
+												>
+													<FontAwesomeIcon icon={faBoxArchive} />
+												</button>
+												<button
+													title="Unpublish"
+													onClick={e => e.stopPropagation()}
+												>
+													<FontAwesomeIcon icon={faEyeSlash} />
+												</button>
+												<button
+													title="Delete"
+													onClick={e => e.stopPropagation()}
+												>
+													<FontAwesomeIcon icon={faTrash} />
+												</button>
+												<button title="Edit" onClick={e => e.stopPropagation()}>
+													<FontAwesomeIcon icon={faPenToSquare} />
+												</button>
+											</div>
 										</div>
-										<div className={profile_css.buttonGroup}>
-											<button
-												title="Archive"
-												onClick={e => {
-													e.stopPropagation();
-													archiveBlog(userData?.user_id, blog.route_id);
-												}}
-											>
-												<FontAwesomeIcon icon={faBoxArchive} />
-											</button>
-											<button
-												title="Unpublish"
-												onClick={e => e.stopPropagation()}
-											>
-												<FontAwesomeIcon icon={faEyeSlash} />
-											</button>
-											<button title="Delete" onClick={e => e.stopPropagation()}>
-												<FontAwesomeIcon icon={faTrash} />
-											</button>
-											<button title="Edit" onClick={e => e.stopPropagation()}>
-												<FontAwesomeIcon icon={faPenToSquare} />
-											</button>
-										</div>
-									</div>
-								</>
-							) : blogs.indexOf(blog) === blogs.length - 1 ? (
-								<h3 className={profile_css.notice}>
-									All of your blogs are archived. Click&nbsp;
-									<Link
-										to={`/user/${userData?.user_id}/profile/settings?section=archived-blogs`}
-									>
-										here
-									</Link>
-									&nbsp;to view them in your settings page
-								</h3>
-							) : null
-						)) || (
-						<h3 className={profile_css.notice}>
-							This user does not have any blogs posted or published
-						</h3>
-					)}
+									</>
+								);
+							})
+						) : (
+							<h3 className={profile_css.notice}>
+								All of your blogs are archived. Click&nbsp;
+								<Link
+									to={`/user/${userData?.user_id}/profile/settings?section=archived-blogs`}
+								>
+									here
+								</Link>
+								&nbsp;to view them in your settings page
+							</h3>
+						)
+						//   || (
+						// 		<h3 className={profile_css.notice}>
+						// 			This user does not have any blogs posted or published
+						// 		</h3>
+						//)
+					}
 				</div>
 			</div>
 		</>
